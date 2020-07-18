054cd5e0b0ed78eb8a550cf0c26905d60d2a67db
Nick Reyno <nickrenoe@gmail.com>
2020-07-18 18:09:19 -0400

    creating API call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super()
+	constructor() {
+		super();
 
-    this.state ={
-      test : "work"
-    }
+		this.state = {
+			test: "work",
+			ingredient: "",
+			meals: [],
+		};
+	}
 
-  }
-      getResults = () => {
-        console.log('working')
-      }
-  render(){
-  return (
-    <div className="App">
-      <button onClick={this.getResults}>a button</button>
-      <h1>{this.state.test}</h1>
-    </div>
-  );
-}
+	updateIngredient = (e) => {
+		this.setState({
+			ingredient: e.target.value,
+		});
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	getResults = (chosenWord) => {
+		chosenWord = "chicken";
+		axios({
+			url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+			method: "GET",
+			params: {
+				i: chosenWord,
+				md: "d",
+			},
+		})
+			// to make sure if the word has definition
+			.then((result) => {
+				console.log("hey this is my .then");
+				result = result.data;
+				this.setState(
+					{
+						meals: result.meals,
+					},
+					() => {
+						console.log(this.state.meals);
+					}
+				);
+			});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<form action="submit" onSubmit={this.handleSubmit}>
+					<input type="text" onChange={this.updateIngredient} />
+					<input
+						type="submit"
+						onClick={() => {
+							this.getResults(this.state.ingredient);
+						}}
+					/>
+				</form>
+				<ul className="gallery">
+					{this.state.meals
+						? this.state.meals.map((i) => {
+								return <li key={i.idMeal}>{i.strMeal}</li>;
+						  })
+						: null}
+				</ul>
+			</div>
+		);
+	}
 }
 export default App;
