35364c76d72c9f5e4f6264ab76b1796b8d48e272
Nick Reyno <nickrenoe@gmail.com>
2020-07-18 17:03:42 -0400

    creating foundation for makeDinner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state ={
+      test : "work"
+    }
+
+  }
+      getResults = () => {
+        console.log('working')
+      }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.getResults}>a button</button>
+      <h1>{this.state.test}</h1>
     </div>
   );
 }
-
+}
 export default App;
